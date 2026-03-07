348d283eaf7d54a02c73ce2b344534ec16ea650e
jaysonCab <jayson.cabrera2002@gmail.com>
2026-03-07 15:42:10 -0500

    Added portion for bcrypt password storage

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const sqlite = require("sqlite");
+const bcrypt = require("bcrypt");
 
 async function init() {
   try {
@@ -15,22 +16,36 @@ async function init() {
 init();
 
 // Grab username and password from database for auth purposes
+// When logging in, ensure you are comparing the password to the hashed stored in database.
 async function authenticate(username, password)
 {
   let result = await db.get(
-    "SELECT * FROM Users WHERE username = ? AND password = ?",
-    [username, password]
+    "SELECT * FROM Users WHERE username = ?",
+    [username]
   );
 
-  return result;
+  if (!result) {
+    return null;
+  }
+
+  const match = await bcrypt.compare(password, result.password);
+
+  if (match) {
+    return result;
+  } else {
+    return null;
+  }
 }
 
 // Create a new user
 async function createUser(username, password, access_level)
 {
+  // Need to hash the password first
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   await db.run(
     "INSERT INTO Users VALUES (?,?,?)",
-    [username, password, access_level]
+    [username, hashedPassword, access_level]
   );
 }
 
